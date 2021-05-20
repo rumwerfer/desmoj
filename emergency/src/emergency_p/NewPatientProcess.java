16e9c3911d24c039d56460e7306e8dcdcb332ed3
Sofia Bonini <sofia.bonini@stud.sbg.ac.at>
2021-05-20 12:27:26 +0200

    new patient process

--- emergency/src/emergency_p/NewPatientProcess.java
+++ emergency/src/emergency_p/NewPatientProcess.java
@@ -1,5 +1,32 @@
 package emergency_p;
 
-public class NewPatientProcess {
+import desmoj.core.simulator.*;
+import emergency_p.Emergency_model;
+import emergency_p.PatientProcess;
+import co.paralleluniverse.fibers.SuspendExecution;
+
+public class NewPatientProcess extends SimProcess {
+    private Emergency_model myModel;
+
+    public NewPatientProcess (Model owner, String name, boolean showInTrace) {
+	   super(owner, name, showInTrace);
+
+	   myModel = (Emergency_model) owner;
+    }
+    
+    public void lifeCycle() throws SuspendExecution {
+	
+        while (true) {
+            // hold this process until new patient arrives
+            hold (new TimeSpan(myModel.getPatientArrivalTime()));
+     
+            // create new patient
+            PatientProcess newPatient = new PatientProcess (myModel, "patient", true);
+    
+            // new patient enters emergency room
+            newPatient.activateAfter(this);
+    
+        }
+    }
 
 }
